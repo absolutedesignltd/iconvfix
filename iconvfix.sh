12989d76b9de5939fcb6637c2421dc537053780a
Dave C <dc@absolute-design.co.uk>
2024-05-30 14:50:57 +0100

    Add script

---
+++ iconvfix.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Checking vuln"
+ISVULN=$(curl -sO https://sansec.io/downloads/cve-2024-2961.c && gcc cve-2024-2961.c -o poc && ./poc)
+echo "Initial check: $ISVULN"
+if [[ "system is vulnerable" == "$ISVULN" ]]; then
+        echo "Proceeding to fix"
+        . /etc/os-release
+        echo "$PRETTY_NAME"
+        case $PRETTY_NAME in
+                "CentOS Linux 7 (Core)")
+                        echo "Centos7 found"
+                        cp /usr/lib64/gconv/gconv-modules /usr/lib64/gconv/gconv-modules.bak
+                        sed -i -e 's/alias	ISO2022CNEXT/#alias	ISO2022CNEXT/g' /usr/lib64/gconv/gconv-modules
+                        sed -i -e 's/module	ISO-2022-CN-EXT/#module	ISO-2022-CN-EXT/g' /usr/lib64/gconv/gconv-modules
+                        sed -i -e 's/module	INTERNAL		ISO-2022-CN-EXT/#module	INTERNAL		ISO-2022-CN-EXT/g' /usr/lib64/gconv/gconv-modules
+                        iconvconfig
+                        echo "Patched and rechecking"
+                        ;;
+                *)
+                        echo "Not match $PRETTY_NAME"
+                        ;;
+        esac
+elif [[ "system is safe" == "$ISVULN" ]]; then
+        echo "Already fixed, exiting"
+else
+        echo "Script issue, check manually"
+fi
+
+ISVULN=$(curl -sO https://sansec.io/downloads/cve-2024-2961.c && gcc cve-2024-2961.c -o poc && ./poc)
+echo "Initial check: $ISVULN"
+if [[ "system is safe" == "$ISVULN" ]]; then
+        echo "Successfully patched"
+else
+        echo "Error in patching, check manually"
+fi
